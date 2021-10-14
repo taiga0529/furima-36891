a503a0035013cf5833addbc90282355d9aa02621
taiga0529 <teega6101@gmail.com>
2021-10-14 23:37:05 +0900

    アソシエーション記述、モデル作成、商品出品画面の表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,16 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def items_params
+    params.require(:item).permit(:name, :image, :text)
+  end
 end
