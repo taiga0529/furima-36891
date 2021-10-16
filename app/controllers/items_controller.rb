2bfecc6693aa1fe9a91e28936a3ffb74662a94ca
taiga0529 <teega6101@gmail.com>
2021-10-16 21:51:51 +0900

    ActiveStorageとActiveHashを導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,16 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.order("created_at DESC")
   end
 
   def new
+    @item = Item.new
   end
 
   def create
-    Item.create(item_params)
+    @item = Item.new(item_params)
+    @article.save
+      redirect_to root_path
   end
 
   private
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,20 @@
 class Item < ApplicationRecord
-  has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :area
+  belongs_to :burden
+  belongs_to :category
+  belongs_to :days
+  belongs_to :status
   belongs_to :user
+  has_one_attached :image
+  
 
   validates :name, presence: true
   validates :price, presence: true
   validates :explanation, presence: true
-  validates :category_id, presence: true
-  validates :status_id, presence: true
-  validates :burden_id, presence: true
-  validates :area_id, presence: true
-  validates :days_id, presence: true
-  validates :user_id, presence: true
+  validates :category_id, numericality: { other_than: 1 ,message: "can't be blank"} 
+  validates :status_id, numericality: { other_than: 1 ,message: "can't be blank"} 
+  validates :burden_id, numericality: { other_than: 1 ,message: "can't be blank"} 
+  validates :area_id, numericality: { other_than: 1 ,message: "can't be blank"} 
+  validates :days_id, numericality: { other_than: 1 ,message: "can't be blank"} 
 end
