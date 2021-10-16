89be501bfdee6c8ceb68d0a751fa9696ef94fa0a
taiga0529 <teega6101@gmail.com>
2021-10-16 18:34:11 +0900

    コントローラーとバリデーションの記述（仮）

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,12 @@
 class Item < ApplicationRecord
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :explanation, presence: true
+  validates :category_id, presence: true
+  validates :status_id, presence: true
+  validates :burden_id, presence: true
+  validates :area_id, presence: true
+  validates :days_id, presence: true
+  validates :user_id, presence: true
   belongs_to :user
 end
