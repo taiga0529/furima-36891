96e532e047b8b20266fe33751fd1d10dc30d051f
taiga0529 <teega6101@gmail.com>
2021-10-17 19:15:21 +0900

    テストコード

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,7 +1,17 @@
 FactoryBot.define do
   factory :item do
-    explanation {Faker::Lorem.sentence}
-    image {Faker::Lorem.sentence}
+    name{ '絵本' }
+    explanation { 'これは絵本です' }
+    price{ '2000' }
+    category_id { '2' }
+    area_id{ '3' }
+    burden_id{ '2' }
+    days_id{ '2' }
+    status_id{ '2' }
     association :user 
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
