35ec7444edc9a65f1a363854a89938bc53e4a328
taiga0529 <teega6101@gmail.com>
2021-10-22 14:49:41 +0900

    修正４

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,17 +1,18 @@
 class PurchaseAddress
   include ActiveModel::Model
   attr_accessor :postcode, :area_id, :address, :telephone_number, :building_name, :purchase_history_id, :municipalities, :user_id, :item_id, :token
-  validates :postcode, presence: true
   validates :postcode,format: { with: /\A\d{3}[-]\d{4}\z/}
   validates :area_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :municipalities,presence: true
-  validates :address, presence: true
   VALID_PHONE_REGEX = /\A\d{10}$|^\d{11}\z/
   validates :telephone_number, presence: true, format: { with: VALID_PHONE_REGEX }
-  validates :user_id, presence: true
-  validates :item_id , presence: true
-  validates :token, presence: true
-
+  with_options presence: true do
+     validates :user_id, presence: true
+     validates :item_id , presence: true
+     validates :token, presence: true
+     validates :municipalities,presence: true
+     validates :address, presence: true
+     validates :postcode, presence: true
+  end
   def save
     purchase_histories = PurchaseHistory.create(item_id: item_id, user_id: user_id)
     Address.create(postcode: postcode, municipalities: municipalities, area_id: area_id, telephone_number: telephone_number, building_name: building_name, address: address, purchase_history_id: purchase_histories.id)
